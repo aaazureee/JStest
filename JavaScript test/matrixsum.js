71349eef52c568faa8380f7a02ba83d1f14ab995
Hieu C. Chu <aaazureee@gmail.com>
2017-04-23 17:28:32 +1000

    format comment

--- JavaScript test/matrixsum.js
+++ JavaScript test/matrixsum.js
@@ -1,5 +1,6 @@
-/* The building is represented by a rectangular matrix of rooms, each cell containing an integer - the price of the room. Some rooms are free (their cost is 0), but that's probably because they are haunted, so all the bots are afraid of them. That is why any room that is free or is located anywhere below a free room in the same column is not considered suitable for the bots.
-
+/* The building is represented by a rectangular matrix of rooms, each cell containing an integer - the price of the room. 
+Some rooms are free (their cost is 0), but that's probably because they are haunted, so all the bots are afraid of them. 
+That is why any room that is free or is located anywhere below a free room in the same column is not considered suitable for the bots.
 Help the bots calculate the total price of all the rooms that are suitable for them. */
 
 var matrix = [[1, 1, 1, 0], [0, 5, 0, 1], [2, 1, 3, 10]];
